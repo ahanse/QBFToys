2a061508f9bd87e3908d811720bd6c7424368f11
Hans-Joerg Schurr <commits@schurr.at>
2014-12-28 13:05:22 +0100

    cmd option -o

--- converter.hs
+++ converter.hs
@@ -1,8 +1,38 @@
 import Qdimacs 
+import System.IO
+import System.Environment
+import Text.Printf
+
+getOutfileName :: IO (Maybe String)
+getOutfileName = do
+    args <- getArgs
+    case args of 
+        ("-o":name:_) -> return (Just name)
+        _ -> return Nothing
+
+cleanQBF :: QBFProblem -> (String, QBFProblem)
+cleanQBF p = (s,p'')
+    where 
+        p' = (normalizeVars.quantifieUndeclaredVars) p
+        (c, p'') = isTrivial p'
+        (nv, nc) = qbfSizeNaive p''
+        s = printf "%d %d %s" nv nc (case c of
+                Nothing -> ""
+                Just True -> "Trivally True"
+                Just False -> "Trivally False")
 
 main :: IO ()
 main = do
     input <- getContents
-    let norm = toQdimacs.normalizeVars.quantifieUndeclaredVars
     let prop = readProblem $ lines input 
-    mapM_ putStrLn $ norm prop
+    let (desc, prop') = cleanQBF prop
+    let output = toQdimacs prop'
+    maybePath <- getOutfileName
+    case maybePath of
+        Just path -> do 
+            handel <- openFile path WriteMode
+            mapM_ (hPutStrLn handel) output 
+            hClose handel
+            putStrLn desc
+        Nothing -> do 
+            mapM_ putStrLn output
