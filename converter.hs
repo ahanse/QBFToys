bbdc84d46464b0679102e0a9e83b03f836925f8a
Hans-Joerg Schurr <hans-joerg@schurr.at>
2015-01-03 21:25:22 +0100

    nice comandline arguments

--- converter.hs
+++ converter.hs
@@ -3,14 +3,30 @@ import Thf
 import System.IO
 import System.Environment
 import Text.Printf
+import System.Console.GetOpt
+import System.Exit
 
-getOutfileName :: IO (Maybe String)
-getOutfileName = do
-    args <- getArgs
-    case args of 
-        ("-o":name:_) -> return (Just name)
-        _ -> return Nothing
+data Flag = Normalize | Output String
+    deriving (Eq,Ord,Show)
 
+
+flags = [Option ['n'] [] (NoArg Normalize)
+            "Outputs a standard complient Qdimacs problem.",
+         Option ['o'] [] (ReqArg Output "FILE")
+            "Output FILE"]
+
+header = unlines ["Usage: convert [-n] [-o FILE]\n",
+         "If an output filename is given a line containing the",
+         "number of variables, clauses and if the problem was",
+         "\"Trivally True\" (contained no clause) or \"Trivally False\"",
+         "(contained the empty clause) is written to stdout."]
+
+getOutfileName :: [Flag] -> Maybe String
+getOutfileName = foldl f Nothing 
+    where f (Just a) _ = Just a
+          f _ (Output s) = Just s
+          f _ _ = Nothing
+    
 cleanQBF :: QBFProblem -> (String, QBFProblem)
 cleanQBF p = (s,p'')
     where 
@@ -24,16 +40,24 @@ cleanQBF p = (s,p'')
 
 main :: IO ()
 main = do
-    input <- getContents
-    let prop = readProblem $ lines input 
-    let (desc, prop') = cleanQBF prop
-    let output = toThf prop'
-    maybePath <- getOutfileName
-    case maybePath of
-        Just path -> do 
-            handel <- openFile path WriteMode
-            mapM_ (hPutStrLn handel) output 
-            hClose handel
-            putStrLn desc
-        Nothing -> do 
-            mapM_ putStrLn output
+    args <- getArgs
+    case getOpt Permute flags args of
+        (args,_,[]) -> do
+            input <- getContents
+            let prop = readProblem $ lines input 
+                (desc, prop') = cleanQBF prop
+                output = (if Normalize `elem` args 
+                            then toQdimacs else toThf) prop'
+                maybePath = getOutfileName args
+            case maybePath of
+                Just path -> do 
+                    handel <- openFile path WriteMode
+                    mapM_ (hPutStrLn handel) output 
+                    hClose handel
+                    putStrLn desc
+                Nothing -> do 
+                    mapM_ putStrLn output
+        (_,_,errs) -> do
+            hPutStrLn stderr (concat errs ++ usageInfo header flags)
+            exitWith (ExitFailure 1)
+
