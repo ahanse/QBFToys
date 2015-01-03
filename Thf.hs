540793b556428e9745e2ea376134871ae8d6d211
Hans-Joerg Schurr <commits@schurr.at>
2015-01-03 19:53:12 +0100

    fixed small bug in thf conv.

--- Thf.hs
+++ Thf.hs
@@ -16,11 +16,15 @@ convertQuant = (intercalate ",").(map (printf "X%d: $o"))
 
 convertTE :: [[Variable]] -> [Clause] -> String
 convertTE [] c = printf "((%s))" (intercalate ")&(" (map convertClause c))
-convertTE (q:t) c = printf "(? [%s]:%s)" (convertQuant q) (convertFA t c)
+convertTE (q:t) c 
+    | q==[] = convertFA t c
+    | otherwise = printf "(? [%s]:%s)" (convertQuant q) (convertFA t c)
 
 convertFA :: [[Variable]] -> [Clause] -> String
 convertFA [] c = printf "((%s))" (intercalate ")&(" (map convertClause c))
-convertFA (q:t) c = printf "(! [%s]:%s)" (convertQuant q) (convertTE t c)
+convertFA (q:t) c 
+    | q==[] = convertTE t c
+    | otherwise = printf "(! [%s]:%s)" (convertQuant q) (convertTE t c)
 
 toThf :: QBFProblem -> [String]
 toThf (v,c) = [printf "thf(c,conjecture,%s)." (convertTE v c)]
