ecaabd9e8abba12dc6acebab297011255e4fde2a
Hans-Joerg Schurr <commits@schurr.at>
2015-04-11 12:16:27 +0900

    benchmarks work now

--- runBenchmarks.py
+++ runBenchmarks.py
@@ -8,6 +8,9 @@ import csv
 import sys
 from subprocess import PIPE, check_output, CalledProcessError, call
 import re
+import time
+
+import scheduler
 
 def readPreprocessorDescription(fileName):
     preprocessors = []
@@ -39,7 +42,7 @@ def readResults(fileName):
             restultReader.next() #skip header
             results=map(lambda x:readResultLine(x), restultReader)
     except IndexError:
-        print "Not enough columngs in restult decleration."
+        print "Not enough columns in result decleration."
         sys.exit(1)
     return results 
 
@@ -58,60 +61,105 @@ def readSolvers(fileName):
 
 def runSolver(solver, benchmarkFile, logFileName, timeout):
     solverCmd = solver.replace("%t", str(timeout)).replace("%f", benchmarkFile)
-    result = 'e'
+    result = 'u'
+    td = 0
     with open(logFileName, 'w') as logFile:
+        ts = time.time()
         returnCode = call(solverCmd, shell=True, stdout=logFile) 
-        if returnCode == 20 or returnCode == 10: # 20 is theorem, 10 count. ex. 
-           result = 's' 
-    return result  
-
-parser = argparse.ArgumentParser(description='Automatically runs a benchmark of HOL and QBF solver.')
-parser.add_argument('input',type=str,
-    help='the folder containing the problems.')
-parser.add_argument("-p", "--preprocessors", type=str, 
-    help="name for csv file containing the used preprocessors (for pretty printing).")
-parser.add_argument("-r", "--results", type=str, 
-    help="CSV file containing the result of the preprocessing.")
-parser.add_argument("-s","--solver", help="CSV file specifying he solvers to use.", type=str)
-parser.add_argument("-v", "--variables", type=str, 
-    help="max. number of variables.")
-parser.add_argument("-c", "--clauses", type=str, 
-    help="max. number of clauses.")
-parser.add_argument("-t", "--timeout", type=str, 
-    help="timeout for each solver in seconds.")
-args = parser.parse_args()
-
-preprocessors = readPreprocessorDescription(args.preprocessors)
-solvers = readSolvers(args.solver)
-results = readResults(args.results)
+        td = time.time() - ts
+        if returnCode == 20: # 20 is theorem, 10 count. ex. 
+           result = 't' 
+        if returnCode == 10:
+           result = 'c'
+    return result, ts
 
 """ States: b...to big
             e...error
-            s...solved
-            u...unsolved """"
+            t...solved
+            c...countersat.
+            u...unsolved 
+            p...preprocessor error """
+def doBenchmark(folder, maxClauses, maxVars, timeout, 
+              benchmarkFilename, variables, clauses, state, solverNmr, cmd):
+    logFileName = "{}_{}.log".format(benchmarkFilename, str(solverNmr))
+    ext = ".thf"
+    if solverNmr==0:
+        ext = ".qdimacs"
+    benchmarkFile=benchmarkFilename+ext
+    if state == "n":
+        if int(clauses) <= maxClauses and int(variables) <= maxVars:
+            print "Running {} on {}.".format(cmd, benchmarkFile)
+            result,t = runSolver(cmd, benchmarkFile, logFileName, timeout)
+            return [state,result,t]
+        else: 
+            print "Skiping {} on {}: Too Big.".format(cmd, benchmarkFile)
+            return [state,'b',0]
+    else:
+        print "Skiping {} on {}.".format(cmd, benchmarkFile)
+        return [state,'e',0]
 
-for p in xrange(len(preprocessors)):
-    folder = os.path.join(args.input, str(p))
-    with open(os.path.join(folder,"results.csv"),"wb") as outputFile:
-        output = csv.writer(outputFile, delimiter=";")
+
+def newRun(args):
+    print "Starting new run."
+    preprocessors = readPreprocessorDescription(args.preprocessors)
+    solvers = readSolvers(args.solver)
+    solvers = [args.qbf]+solvers
+    solvers = zip(xrange(len(solvers)),solvers)
+    results = readResults(args.results)
+
+    tools = []
+    for p in xrange(len(preprocessors)):
+        folder = os.path.join(args.input, str(p))
+        tool = scheduler.Tool(os.path.join(folder, "result.csv"),
+            [folder, args.clauses, args.variables, args.timeout])
+
         for benchmark in results:
             filename = benchmark[0]
             benchmarkFile = os.path.join(folder, filename)
             benchmarkFilename, benchmarkFileExtension = os.path.splitext(benchmarkFile)
-            solverResult = []
-            for s in xrange(len(solvers)):
-                solver = solvers[s]
-                logFileName = "{}_{}.log".format(benchmarkFilename, str(s))
-                (variables, clauses, state) = benchmark[p+1]
-                if state == "n":
-                    if clauses <= args.clauses and variables <= args.variables:
-                        print "Running {} on {}.".format(solver, benchmarkFile)
-                        result = runSolver(solver, benchmarkFile, logFileName, timeout)
-                        solverResult.extend([state,result])
-                    else: 
-                        print "Skiping {} on {}: To Big.".format(solver, benchmarkFile)
-                        solverResult.extend([state,'b'])
-                else:
-                    print "Skiping {} on {}.".format(solver, benchmarkFile)
-                    solverResult.extend([state,'e'])
-            output.write(solverResult)
+            (variables, clauses, state) = benchmark[p+1]
+            
+            instance = scheduler.Instance(filename, 
+                [benchmarkFilename, variables, clauses, state],solvers)
+            tool.addInstance(instance)
+        tools.append(tool)
+    sche = scheduler.Scheduler(doBenchmark,tools, args.maxtemp,args.continuetemp, args.autosave)
+    sche.run()
+
+def resumeRun(args):
+    print "Restoring from", args.savedState
+    sche = scheduler.restoreScheduler(args.savedState)
+    print "Resuming"
+    sche.run()
+
+parser = argparse.ArgumentParser(description='Automatically runs a benchmark of HOL and QBF solver.')
+subparsers = parser.add_subparsers()
+
+parser_start =  subparsers.add_parser('start')
+parser_start.add_argument('input',type=str,
+    help='the folder containing the problems.')
+parser_start.add_argument("-p", "--preprocessors", type=str, 
+    help="name for csv file containing the used preprocessors (for pretty printing).")
+parser_start.add_argument("-r", "--results", type=str, 
+    help="CSV file containing the result of the preprocessing.")
+parser_start.add_argument("-s","--solver", help="CSV file specifying he solvers to use.", type=str)
+parser_start.add_argument("-v", "--variables", type=int, 
+    help="max. number of variables.")
+parser_start.add_argument("-c", "--clauses", type=int, 
+    help="max. number of clauses.")
+parser_start.add_argument("-t", "--timeout", type=str, 
+    help="timeout for each solver in seconds.")
+parser_start.add_argument("-q", "--qbf", type=str, 
+    help="QBF solver used as reference")
+parser_start.add_argument("-m","--maxtemp", help="If the temperature is reached, the script waits until the temperature is below 'continuetemp'", type=int, default=0)
+parser_start.add_argument("-o","--continuetemp", help="If the temperature falls bellow this value, the script is continued.", type=int, default=0)
+parser_start.add_argument("-a","--autosave", help="Saves the state all N converted files.", type=int, default=0)
+parser_start.set_defaults(func=newRun)
+
+parser_resume =  subparsers.add_parser('resume')
+parser_resume.add_argument('savedState',type=str,
+    help='File containing a previously saved state.')
+parser_resume.set_defaults(func=resumeRun)
+
+args = parser.parse_args()
+args.func(args)
