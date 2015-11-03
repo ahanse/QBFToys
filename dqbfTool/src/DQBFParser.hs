b1c646c3cf7e5de1b7d452ca4f38b1cadaf45064
Hans-Joerg Schurr <commits@schurr.at>
2015-11-03 18:56:37 +0100

    more unit tests + bugfixes resulting from the unit tests

--- dqbfTool/src/DQBFParser.hs
+++ dqbfTool/src/DQBFParser.hs
@@ -34,29 +34,25 @@ varList = many m_identifier
 
 allQuantifier :: Parser AQuant
 allQuantifier = do
-  char 'A'
-  m_whiteSpace
+  char 'A' >> m_whiteSpace
   vars <- varList
-  char ':'
-  m_whiteSpace
+  char ':' >> m_whiteSpace
   return (AQuant vars)
 
 existsQuantifier :: Parser EQuant
 existsQuantifier = do
-  char 'E'
-  m_whiteSpace
+  char 'E' >> m_whiteSpace
   deps <- m_braces varList
   vars <- varList
-  char ':'
-  m_whiteSpace
+  char ':' >> m_whiteSpace
   return (EQuant deps vars)
 
 aliasedOp l = choice $ fmap m_reservedOp l
 
 expressionTable = [
   [Prefix (aliasedOp notS >> return Not)],
-  [Infix (aliasedOp andS >> return And) AssocLeft],
   [Infix (aliasedOp orS >> return Or) AssocLeft],
+  [Infix (aliasedOp andS >> return And) AssocLeft],
   [Infix (aliasedOp ifS >> return If) AssocLeft],
   [Infix (aliasedOp xorS >> return Xor) AssocLeft],
   [Infix (aliasedOp iffS >> return Iff) AssocLeft]]
@@ -72,5 +68,4 @@ dqbfParser = do
   existsQ <- many1 existsQuantifier
   m_whiteSpace
   expr <- expression
-  eof
-  return (allQ, existsQ, expr)
+  eof >> return (allQ, existsQ, expr)
