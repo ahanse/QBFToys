6090cc3939a164d8dc1c8078c23731e33a2e43b3
Hans-Jörg Schurr <hans-joerg@schurr.at>
2015-11-04 13:36:43 +0100

    ?

--- dqbfTool/test/DQBFParserSpec.hs
+++ dqbfTool/test/DQBFParserSpec.hs
@@ -35,7 +35,7 @@ spec = let
     ("E {} y1 : E {} y2 y3: E {}:",[EQuant [] ["y1"], EQuant [] ["y2","y3"], EQuant [] []])] ++ (zip simpExists $ cycle [simpExistsP])
 
   parserInstanceSpecs (l,r) = it ("parses: "++show l) $ parse dqbfParser "" l `shouldBe` (Right r)
-  in 
+  in
   describe "dqbfParser" $ do
     it "parses simple formula" $
       parse dqbfParser "" "A x1 : E {x1} y1 : x1 /\\ y1" `shouldBe` Right ([AQuant ["x1"]], [EQuant ["x1"] ["y1"]], And (Var "x1") (Var "y1"))
