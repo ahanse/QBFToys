dc7bc045defc834f882f4dfb46ea8bbf986b6e17
Hans-Jörg Schurr <commits@schurr.at>
2017-09-16 21:29:09 +0200

    Remove old material and cleanup

--- scripts/run_bench.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -u ./QBFToys/runBenchmarks.py start -p "Benchmarks/preprocessors.csv" -r "Benchmarks/results.csv" -s "Benchmarks/solvers.csv" -v 2500 -c 2500 -t 120 -q "./timeout -c -m 7340032 ./depqbf --max-secs=%t %f" -a 0 Benchmarks/BenchmarkFiles/output
